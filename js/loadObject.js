b6c5b702d63ee43417a06f3704df624c315a4531
octopi <dackogan@gmail.com>
2021-10-19 23:36:13 +0700

    import model via GLTFLoader

--- js/loadObject.js
+++ js/loadObject.js
@@ -8,8 +8,13 @@ document.body.appendChild(renderer.domElement)
 var scene = new THREE.Scene();
 scene.background = new THREE.Color(0x334756);
 //NOTE setup camera
-var camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+
+
+camera.position.x = 3;
+camera.position.y = 3;
+camera.position.z = 10;
 
 
 //setup resizing window
@@ -22,13 +27,28 @@ window.addEventListener('resize', ()=> {
     camera.updateProjectionMatrix();
 })
 
+
+
 //NOTE setup control
 controls = new THREE.OrbitControls(camera, renderer.domElement)
 
 
+hlight = new THREE.AmbientLight (0x404040,8);
+scene.add(hlight);
+
+var model;
+let loader = new THREE.GLTFLoader();
+loader.load('models/Tiger_Head_Bottle.gltf', function(gltf){
+    
+  gltf.scene.position.x = -2.5;
+  gltf.scene.position.y = 0.3;
+  model = gltf.scene;
+  scene.add(model);
+});
+
 
 //create the shape
-var geometry = new THREE.BoxGeometry(1.6,1,1);
+var geometry = new THREE.BoxGeometry(6,3.5,3.5);
 var cubeMaterials = [
     new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("img/dark_beer_side_HD.png"), side: THREE.DoubleSide }), // x axis left
     new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("img/dark_beer_side_HD.png"), side: THREE.DoubleSide }), // x axis right
@@ -41,23 +61,21 @@ var cubeMaterials = [
 // var material = new THREE.MeshBasicMaterial({color: 0xF0A500, wireframe: true});
 var material = new THREE.MeshFaceMaterial(cubeMaterials);
 var cube = new THREE.Mesh( geometry, material);
+cube.position.x = 2
+cube.position.z = -1.5
 
 scene.add(cube);
-camera.position.z = 4;
+
 
 
 //update animation here
 var updateAnimation = function (){
     // cube.rotation.x += .01;
     cube.rotation.y += .005;
-    cube.rotation.x = .27;
-    // cube.scale.x += .001;
-    // cube.rotation.y += .01;
-
-    //TODO rotate camera around the cube || scene
-    // camera.position.x += 0.01;
-    // camera.lookAt(scene.position);
+    model.rotation.y += .005;
 }
+
+
 
 //draw scene
 var render = function(){
